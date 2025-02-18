02dab0b8180c6ee4f0380e62ef9396c6efce08b4
Carlos Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2025-02-17 21:57:00 -0500

    Add fields to users table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -10,28 +10,69 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Section;
+use Illuminate\Support\Collection;
+use App\Models\Country;
+use App\Models\State;
+use App\Models\City;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
+    protected static ?string $navigationLabel = 'Employees';
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required(),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required(),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required(),
+                Section::make('Personal Information')
+                    ->description('Enter the personal information of the employee.')
+                    ->columns(3)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')->required(),
+                        Forms\Components\TextInput::make('email')->email()->required(),
+                        Forms\Components\TextInput::make('password')->password()->required(),
+                    ]),
+                Section::make('Address Information')
+                    ->description('Enter the address information of the employee.')
+                    ->columns(3)
+                    ->schema([
+                        Forms\Components\Select::make('country_id')
+                            ->relationship(name: 'country', titleAttribute: 'name')
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->afterStateUpdated(function (Set $set){
+                                $set('state_id', null);
+                                $set('city_id', null);
+                            })
+                            ->required(),
+                        Forms\Components\Select::make('state_id')
+                            ->label('State')
+                            ->options(fn (Get $get): Collection => State::query()
+                                        ->where('country_id', $get('country_id'))
+                                        ->pluck('name', 'id'))
+                            ->searchable()
+                            ->preload()
+                            ->afterStateUpdated(function (Set $set){
+                                $set('city_id', null);
+                            })
+                            ->required(),
+                        Forms\Components\Select::make('city_id')
+                            ->label('City')
+                            ->options(fn (Get $get): Collection => City::query()
+                                        ->where('state_id', $get('state_id'))
+                                        ->pluck('name', 'id'))
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                        Forms\Components\TextInput::make('address')->required(),
+                        Forms\Components\TextInput::make('postal_code')->required(),
+                    ]),
             ]);
     }
 
